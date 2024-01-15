af9b87ba72f9eca5731e6815f1b1b4879396a609
Marosi Gergely <gergely.marosi@accedo.tv>
2024-01-15 22:05:00 +0100

    fish setup

--- macOS/print.sh
+++ macOS/print.sh
@@ -1,32 +1,36 @@
 #!/usr/bin/env bash
 
 message () {
-  printf "  $1\n"
+  printf "\r\033[0m  $1\n"
 }
 
 warn () {
-  printf "\r\033[1;35m!\033[0;1m $1\n"
+  printf "\r\033[0;35m!\033[0m $1\n"
 }
 
 question () {
-  printf "\r\033[1;33m?\033[0;1m $1\n"
+  printf "\r\033[0;33m?\033[0m $1\n"
 }
 
 task () {
-  printf "\r\033[1;34m.\033[0;1m $1"
+  printf "\r\033[5;34m.\033[0m $1"
 }
 
 success () {
-  printf "\r\033[2K\033[1;32m\xE2\x9C\x94\033[0;1m $1\n"
+  printf "\r\033[2K\033[0;32m\xE2\x9C\x94\033[0m $1\n"
 }
 
 fail () {
-  printf "\r\033[2K\033[1;31mx\033[0;1m $1\n"
+  printf "\r\033[2K\033[0;31mx\033[0m $1\n"
+  if [ "$1" = "-x" ]; then
+    echo ''
+    exit
+  fi
 }
 
 confirm() {
-  printf "\r\033[1;33m? \033[0;1m$1\033[0m"
-  read -r -p " [Y/n] " response
+  printf "\r\033[0;33m?\033[0m $1"
+  read -r -p $'\033[0;1m [Y/n] \033[0m' response
   case "$response" in
       [yY][eE][sS]|[yY])
           true
--- macOS/sandbox.sh
+++ macOS/sandbox.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 source ./print.sh
+#source /dev/stdin <<< "$(curl -s --insecure https://raw.githubusercontent.com/marosige/I-like-it-like-that/main/macOS/print.sh)"
 
 message "Message"
 warn "Warn"
@@ -9,3 +10,5 @@ task "Task"; printf "\n"
 success "Success"
 fail "Fail"
 confirm "Confirm?"
+
+echo "${BASH_VERSION}"
