66a8e4344c95e7a9193addb99b978d2f5e03c3cf
Marosi Gergely <gergely.marosi@accedo.tv>
2024-01-25 11:12:28 +0100

    sys

--- macOS/preferences/system.sh
+++ macOS/preferences/system.sh
@@ -3,8 +3,10 @@
 ###############################################################################
 # System
 ###############################################################################
+exit=0
 
 # Set computer name
-sudo scutil --set ComputerName "gengar"
-sudo scutil --set LocalHostName "gengar"
- 
+sudo scutil --set ComputerName "gengar" || exit=1
+sudo scutil --set LocalHostName "gengar" || exit=1
+
+exit $exit
